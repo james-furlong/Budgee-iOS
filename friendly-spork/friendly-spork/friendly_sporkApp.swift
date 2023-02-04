094e3689131cefbc087893ba4f20eed0223860b5
James Furlong <19883419+james-furlong@users.noreply.github.com>
2023-02-04 13:02:09 +1100

    Initial setup commit

---
+++ friendly-spork/friendly-spork/friendly_sporkApp.swift
@@ -0,0 +1,17 @@
+//
+//  friendly_sporkApp.swift
+//  friendly-spork
+//
+//  Created by James on 4/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct friendly_sporkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
